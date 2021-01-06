127edaa01b53e9bfff4aeedacfbdee8f967f0b82
peter <peter@aa.com>
2021-01-06 11:43:56 +0800

    puppy

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import main from '@/components/main'
+import puppy from '@/components/puppy'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/main',
       name: 'main',
       component: main
+    },
+ {
+      path: '/puppy',
+      name: 'puppy',
+      component:puppy
     }
   ]
 })
